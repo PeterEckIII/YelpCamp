ff65a0be89fd42c9941c528482d637baf3cb712d
Peter Eck <jpeckiii@gmail.com>
2018-06-02 12:56:56 -0500

    Corrected error that wasn't allowing routes to form pages

--- middleware/index.js
+++ middleware/index.js
@@ -38,12 +38,10 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
 }
 
 middlewareObj.isLoggedIn = function(req, res, next) {
-    function isLoggedIn(req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect("/login");
+    if (req.isAuthenticated()) {
+        return next();
     }
+    res.redirect("/login");
 }
 
 module.exports = middlewareObj
